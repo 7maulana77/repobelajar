0794e46fd39d03927f3ee4ec351001a68bb52967
7maulana77 <7maulana77@gmail.com>
2021-06-09 15:20:10 +0700

    mencoba tugas 3

---
+++ hari3/Tugas-3/tugas3.js
@@ -0,0 +1,14 @@
+var kataPertama = 'saya'
+var kataKedua = 'senang'
+var kataKetiga = 'belajar'
+var kataKeempat = 'javascript'
+
+console.log(
+    kataPertama.concat(
+        kataKedua[0].toUpperCase(),
+        kataKedua.substring(1),
+        kataKetiga.substr(0,kataKetiga.length-1),
+        kataKetiga[6].toUpperCase(),
+        kataKeempat.toUpperCase()
+        )
+    )
