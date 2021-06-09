e927cc3393f96fc2d4202b6fec70035188d04fbc
7maulana77 <7maulana77@gmail.com>
2021-06-09 15:48:21 +0700

    menambah white space antar kata soal1

--- hari3/Tugas-3/tugas3.js
+++ hari3/Tugas-3/tugas3.js
@@ -1,14 +1,15 @@
+//Soal 1
 var kataPertama = 'saya'
 var kataKedua = 'senang'
 var kataKetiga = 'belajar'
 var kataKeempat = 'javascript'
 
 console.log(
-    kataPertama.concat(
+    kataPertama.concat(" ",
         kataKedua[0].toUpperCase(),
-        kataKedua.substring(1),
+        kataKedua.substring(1)," ",
         kataKetiga.substr(0,kataKetiga.length-1),
-        kataKetiga[6].toUpperCase(),
+        kataKetiga[6].toUpperCase()," ",
         kataKeempat.toUpperCase()
-        )
     )
+)
