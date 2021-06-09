f6f09b216cf38d241ce63c09e3a65c113a5fe5ee
7maulana77 <7maulana77@gmail.com>
2021-06-09 15:20:36 +0700

    commit latihan tugas 2

---
+++ hari3/index.js
@@ -0,0 +1,56 @@
+var sayHello = "Hello World!"
+console.log(sayHello)
+
+
+
+var nama = "John" //Tipe
+var angka =12
+var trueataufalse=false
+console.log(angka)
+console.log(trueataufalse)
+console.log(nama)
+
+var testakdefine
+console.log(testakdefine)
+
+console.log(trueataufalse.length)
+console.log('trueataufalse'.charAt(2));
+
+var string1='abdul'
+var string2='manaf'
+console.log(string1.concat(string2))
+
+var indeksof = 'Bom bom paw'
+console.log(indeksof.indexOf('paw'));
+console.log(indeksof.indexOf('o'));
+console.log(indeksof.indexOf('dum'));
+
+var suuubstriing1 ='suuub striing1'
+var strii = console.log(suuubstriing1.substring(6,11))
+
+var int=12
+var real=3.45
+var arr=[6, 7, 8]
+
+var strInt= String(int)
+var strReal= String(real)
+var strArr= String(arr)
+
+console.log(strInt)
+console.log(strReal)
+console.log(strArr)
+
+console.log(int)
+console.log(real)
+console.log(arr)
+
+console.log(trueataufalse.toString())
+console.log(trueataufalse)
+
+var buttonPushed = 1;
+switch(buttonPushed) {
+  case 1:   { console.log('matikan TV!'); break; }
+  case 2:   { console.log('turunkan volume TV!'); break; }
+  case 3:   { console.log('tingkatkan volume TV!'); break; }
+  case 4:   { console.log('matikan suara TV!'); break; }
+  default:  { console.log('Tidak terjadi apa-apa'); }}
