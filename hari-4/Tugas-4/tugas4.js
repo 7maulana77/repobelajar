670125a76c79db52a49d423bb9b79b0d36668e9b
7maulana77 <7maulana77@gmail.com>
2021-06-10 11:16:58 +0700

    mengerjakan soal-1

---
+++ hari-4/Tugas-4/tugas4.js
@@ -0,0 +1,13 @@
+// Soal 1
+nomor=2
+console.log("LOOPING Pertama")
+while (nomor<=20){
+    console.log(nomor+" - I love coding" )
+    nomor = nomor+2
+}
+console.log("LOOPING Kedua")
+while (nomor>=2){
+    nomor = nomor-2
+    console.log(nomor+" - I will become a frontend developer" )
+}
+//Soal 2
